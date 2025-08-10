8e836c73865f5400805da00e58866f853ebe32c7
mohamed_ezz <mohamed_ezz@gmail.com>
2025-08-10 05:46:07 +0300

    Q1

--- C44-G03-CS07/Program.cs
+++ C44-G03-CS07/Program.cs
@@ -1,10 +1,31 @@
 ﻿namespace C44_G03_CS07
 {
+    #region Q01
+    //public struct Person
+    //{
+    //    public string Name { get; set; }
+    //    public int Age { get; set; }
+
+    //}
+    #endregion
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region Q1.Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.
+            //Person[] people =
+            //{
+            //    new Person { Name = "Ahmed", Age = 25 },
+            //    new Person { Name = "Sara", Age = 30 },
+            //    new Person { Name = "Mona", Age = 22 }
+            //};
+
+            //for (int i = 0; i < people.Length; i++)
+            //{
+            //    Console.WriteLine($"Person {i + 1}: Name = {people[i].Name}, Age = {people[i].Age}");
+            //}
+            #endregion
         }
     }
 }

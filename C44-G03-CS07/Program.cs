bdada8c954a0cad642aa0ef3e543d443e652c1fd
mohamed_ezz <mohamed_ezz@gmail.com>
2025-08-10 06:10:11 +0300

    V03

--- C44-G03-CS07/Program.cs
+++ C44-G03-CS07/Program.cs
@@ -63,6 +63,60 @@
     //}
 
     #endregion
+
+    #region V03
+
+    internal struct Point
+    {
+        #region Attributes
+
+        public int X;
+
+        public int Y;
+
+        #endregion
+
+        #region Constructors
+        // It Is Special Function
+        // 1. Becouse Name As Struct Name
+        // 2. Has No Return Type
+
+        // He Build This Function By him Self
+        public Point()
+        {
+            X = default;
+            Y = default;
+        }
+
+        public Point(int _x, int _y)
+        {
+            X = _x;
+            Y = _y;
+        }
+
+        public Point(int Number)
+        {
+            X = Y = Number;
+        }
+        #endregion
+
+
+
+        #region Methods
+
+        public override string ToString()
+        {
+            return $"({X} , {Y})";
+        }
+
+        #endregion
+
+
+    }
+
+
+
+    #endregion
     internal class Program
     {
         static void Main(string[] args)
@@ -119,6 +173,39 @@
 
             //Console.WriteLine(P01.X);
             //Console.WriteLine(P01.Y); 
+            #endregion
+
+            #endregion
+
+            #endregion
+
+            #region V03
+
+            #region Struct
+
+            #region Example 02
+
+            //Point P01 = new Point(10,20);
+            //Console.WriteLine(P01.X);
+            //Console.WriteLine(P01.Y);
+
+            //Point P02 = new Point(30, 40);
+            //Console.WriteLine(P02.X);
+            //Console.WriteLine(P02.Y);
+
+            //P02 = P01;
+            //Console.WriteLine(P02.X);
+            //Console.WriteLine(P02.Y);
+
+            //P01.X = 500;
+            //Console.WriteLine(P01.X);
+            //Console.WriteLine(P02.X);
+
+            //int X = 10;
+            //Console.WriteLine(X);
+            //Console.WriteLine(P01); //Demo.Point
+            //Console.WriteLine(P01.ToString()); //Demo.Point
+
             #endregion
 
             #endregion

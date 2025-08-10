f3a28d242b2215ede2df08684cf970c21367883d
mohamed_ezz <mohamed_ezz@gmail.com>
2025-08-10 05:47:48 +0300

    Q2

--- C44-G03-CS07/Program.cs
+++ C44-G03-CS07/Program.cs
@@ -9,6 +9,29 @@
     //}
     #endregion
 
+    #region Q02
+
+    //public struct Point
+    //{
+    //    public int X { get; set; }
+
+    //    public int Y { get; set; }
+
+    //    public Point( int _X , int _Y )
+    //    {
+    //        X = _X; 
+    //        Y = _Y;
+    //    }
+
+    //    public override string ToString()
+    //    {
+    //        return $"Point ({X} , {Y})";
+    //    }
+
+    //}
+
+    #endregion
+
     internal class Program
     {
         static void Main(string[] args)
@@ -25,6 +48,43 @@
             //{
             //    Console.WriteLine($"Person {i + 1}: Name = {people[i].Name}, Age = {people[i].Age}");
             //}
+            #endregion
+
+            #region Q2.Create a struct called "Point" to represent a 2D point with properties "X" and   "Y". Write a C# program that takes two points as input from the user and calculates the distance between them.
+            //Point[] p = new Point[2];
+            //int x;
+            //int y;
+            //bool Flag_X;
+            //bool Flag_Y;
+            //for (int i = 0; i < p.Length; i++)
+            //{
+            //    Console.WriteLine($"Point Number {i+1}");
+
+            //    do
+            //    {
+            //        Console.Write("Enter X : ");
+            //        Flag_X = int.TryParse(Console.ReadLine(), out x);
+            //    }
+            //    while (!Flag_X);
+
+
+            //    do
+            //    {
+            //        Console.Write("Enter Y : ");
+            //        Flag_Y = int.TryParse(Console.ReadLine(), out y);
+            //    }
+            //    while (!Flag_Y);
+
+            //    p[i] = new Point(x, y);
+
+
+            //}
+
+            //double distance = Math.Sqrt(Math.Pow(p[0].X - p[1].X,2) + Math.Pow(p[0].Y - p[1].Y, 2));
+
+            //Console.WriteLine($"First Point : {p[0]}");
+            //Console.WriteLine($"Second Point : {p[1]}");
+            //Console.WriteLine($"The Distance Is {distance}");
             #endregion
         }
     }
